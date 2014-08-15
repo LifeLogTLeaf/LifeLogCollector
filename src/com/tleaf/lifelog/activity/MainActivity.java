ca0ee5b876d80bc68e96518a46ac5ff14c964908
titf1011 <jangyoungjin@JangYoungjins-MacBook-Pro.local>
2014-08-15 15:19:04 +0900

    Young made a hashKey method in MainActivity.java

--- src/com/tleaf/lifelog/activity/MainActivity.java
+++ src/com/tleaf/lifelog/activity/MainActivity.java
@@ -1,15 +1,24 @@
 package com.tleaf.lifelog.activity;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Base64;
+import android.util.Log;
 
 import com.tleaf.lifelog.R;
 import com.tleaf.lifelog.fragment.LoginFragment;
 import com.tleaf.lifelog.util.Mylog;
 
-public class MainActivity extends FragmentActivity{
+public class MainActivity extends FragmentActivity {
 	private static final String TAG = "MAIN ACTIVITY";
 	private LoginFragment mLoginFragment;
 	private FragmentManager mFragmentManager;
@@ -20,17 +29,18 @@ public class MainActivity extends FragmentActivity{
 		Mylog.i(TAG, "Created MainActivity");
 		setContentView(R.layout.activity_main);
 		init();
-		
-		//멘인액티비티에서 모든일을 다 끝내고 최초 시작할 프래그먼트를 선택한다.
-		//이때 로그인이 되있으면 메인프래그먼트를 안되있으면 로그인프래그먼트를 부른다.
-		//SharedPreference에 저장하고 관리할 예정
-		//---지금은 생략---
+		loadHashKey();
+		// 멘인액티비티에서 모든일을 다 끝내고 최초 시작할 프래그먼트를 선택한다.
+		// 이때 로그인이 되있으면 메인프래그먼트를 안되있으면 로그인프래그먼트를 부른다.
+		// SharedPreference에 저장하고 관리할 예정
+		// ---지금은 생략---
 		/*
-		if(true){
-			
-		} else {
-			
-		}*/
+		 * if(true){
+		 * 
+		 * } else {
+		 * 
+		 * }
+		 */
 	}
 
 	private void init() {
@@ -39,5 +49,23 @@ public class MainActivity extends FragmentActivity{
 		LoginFragment loginFragment = new LoginFragment(mFragmentManager);
 		ft.add(R.id.fragment_container, loginFragment);
 		ft.commit();
+	}
+
+	private void loadHashKey() {
+		// Add code to print out the key hash
+		try {
+			PackageInfo info = getPackageManager().getPackageInfo(
+					getPackageName(), PackageManager.GET_SIGNATURES);
+			for (Signature signature : info.signatures) {
+				MessageDigest md = MessageDigest.getInstance("SHA");
+				md.update(signature.toByteArray());
+				Log.d("KeyHash:",
+						Base64.encodeToString(md.digest(), Base64.DEFAULT));
+			}
+		} catch (NameNotFoundException e) {
+
+		} catch (NoSuchAlgorithmException e) {
+
+		}
 	}
 }
